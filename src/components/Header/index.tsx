cf556ad428245ffb48e31a663b3b7d45b45e1040
Eduardo Borges <92747098+eduardo-borges-goncalves@users.noreply.github.com>
2022-07-15 14:22:12 -0300

    implement logout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,40 +1,51 @@
 import { Menu } from 'antd';
 import { Link } from 'react-router-dom';
 import { useAuthentication } from '../../contexts/Authentication';
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom"
 
 export const Header = () => {
-  const { isAuthenticated } = useAuthentication()
+  const { isAuthenticated, setIsAuthenticated } = useAuthentication()
+
+  const navigate = useNavigate()
+  function logout() {
+    setIsAuthenticated(false)
+    Cookies.remove("access_token"); 
+    Cookies.remove("name"); 
+    Cookies.remove("id"); 
+    navigate("/login")
+  }
 
   return (
     <Menu mode="horizontal" defaultSelectedKeys={[`${window.location.pathname}`]}
       style={{
-        justifyContent: "flex-end", 
+        justifyContent: "flex-end",
         padding: "0.125rem",
       }}
     >
       <Menu.Item key="/" >
-        <Link to="/"/>
+        <Link to="/" />
         Home Page
       </Menu.Item>
       <Menu.Item key="/sales-orders" >
-        <Link to="/sales-orders"/>
+        <Link to="/sales-orders" />
         Pedidos de Venda
       </Menu.Item>
       <Menu.Item key="/products">
-        <Link to="/products"/>
+        <Link to="/products" />
         Produtos
       </Menu.Item>
       <Menu.Item key="/clients" >
-        <Link to="/clients"/>
+        <Link to="/clients" />
         Clientes
       </Menu.Item>
       <Menu.Item key="/users" >
-        <Link to="/users"/>
+        <Link to="/users" />
         Usuários
       </Menu.Item>
       <Menu.Item key="login" >
-        <Link to="/login"/>
-        {isAuthenticated? "Sair" : "Login"}
+        <Link to="/login" onClick={logout}/>
+          {isAuthenticated ? "Sair" : "Login"}
       </Menu.Item>
     </Menu>
   )
